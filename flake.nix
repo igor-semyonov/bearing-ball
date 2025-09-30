5d1866d3c4ff4a50511aba6a6ba7a8506c64cc2e
Igor Semyonov <igor@semyonov.xyz>
2025-09-29 22:07:01 -0400

    switched to flake managed toolchain

--- flake.nix
+++ flake.nix
@@ -3,23 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    fenix,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
     };
+    fenixLib = fenix.packages.${system};
+    rustToolchain = fenixLib.complete.toolchain;
   in {
     devShells.${system}.default = pkgs.mkShell rec {
       nativeBuildInputs = with pkgs; [
         pkg-config
       ];
       buildInputs = with pkgs; [
+        rustToolchain
         udev
         alsa-lib-with-plugins
         vulkan-loader
